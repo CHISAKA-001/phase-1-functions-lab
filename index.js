3752428e4c1bd571cfecba1dd1ded456c12bc47b
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-16 20:51:22 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+//distanceFromHqInBlocks()
+function distanceFromHqInBlocks(blockDistance){
+    if (blockDistance > 42) {
+        return blockDistance - 42;
+    } else {
+        return 42 - blockDistance;
+    }
+}
+
+//distanceFromHqInFeet()
+function distanceFromHqInFeet (blockDistance) {
+    return distanceFromHqInBlocks(blockDistance) * 264;
+}
+
+//distanceTravelledInFeet()
+ function distanceTravelledInFeet(start, destination) {
+    return start < destination ? (destination -start) * 264 :(start -destination) * 264;
+ }
+
+ //calculatesFarePrice(start, destination)
+ function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+
+
